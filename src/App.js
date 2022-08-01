f2fd98d043fffde2396853c511d9fdd677785b1e
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-08-01 09:43:31 +0430

    Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./Components/DetailPage/DetailPage";
 import HomePage from "./Components/HomePage/HomePage";
+import { useRecoilState } from "recoil";
+import { TodosAtom } from "./Components/todos.atom";
 
 const App = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [_, setTodos] = useRecoilState(TodosAtom);
+
+  const fetchData = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    setTodos(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <h1>State Management Battle</h1>
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,31 +1,40 @@
 import React from "react";
 import Item from "../Item/Item";
+import { useRecoilState } from "recoil";
+import { TodosAtom } from "../todos.atom";
 
-const Mock = [
-  {
-    id: 1,
-    title: "delectus aut autem",
-    completed: false,
-  },
-  {
-    id: 2,
-    title: "quis ut nam facilis et officia qui",
-    completed: true,
-  },
-  {
-    id: 3,
-    title: "fugiat veniam minus",
-    completed: false,
-  },
-];
 const HomePage = () => {
+  const [Todos, setTodos] = useRecoilState(TodosAtom);
+
+  const changeState = (id) => {
+    const newTodos = Todos.map((td) => {
+      if (td.id === id) {
+        return {
+          ...td,
+          completed: !td.completed,
+        };
+      } else {
+        return td;
+      }
+    });
+
+    setTodos(newTodos);
+  };
+
+  const DeleteItem = (id) => {
+    const newTodos = Todos.filter((td) => td.id !== id);
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <p>To do :</p>
 
-      {Mock.filter((item) => !item.completed && item).map((item) => {
+      {Todos.filter((item) => !item.completed && item).map((item) => {
         return (
           <Item
+            deleteItem={DeleteItem}
+            toggle={changeState}
             id={item.id}
             title={item.title}
             completed={item.completed}
@@ -34,9 +43,11 @@ const HomePage = () => {
         );
       })}
       <p>Done :</p>
-      {Mock.filter((item) => item.completed && item).map((item) => {
+      {Todos.filter((item) => item.completed && item).map((item) => {
         return (
           <Item
+            deleteItem={DeleteItem}
+            toggle={changeState}
             id={item.id}
             title={item.title}
             completed={item.completed}
