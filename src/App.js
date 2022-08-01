91e2f6c226891ab245c153784a92d870a8bfc983
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-08-01 23:16:51 +0430

    initialize mobx

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./Components/DetailPage/DetailPage";
 import HomePage from "./Components/HomePage/HomePage";
+import { observer } from "mobx-react";
 
 const App = () => {
   return (
