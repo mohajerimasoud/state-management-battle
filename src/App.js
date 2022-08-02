08196ca042f565efeec88d98530714b2581f9ce3
Masoud Mohajeri <masoudmohajeri.wd@gmail.com>
2022-08-02 11:51:05 +0430

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import DetailPage from "./Components/DetailPage/DetailPage";
 import HomePage from "./Components/HomePage/HomePage";
+import { useTodosStore } from "./Components/store.zustand";
 
 const App = () => {
+  const fetchData = useTodosStore((state) => state.fetchData);
+
+  useEffect(() => {
+    fetchData();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="App">
       <h1>State Management Battle</h1>
--- src/Components/DetailPage/DetailPage.jsx
+++ src/Components/DetailPage/DetailPage.jsx
@@ -1,13 +1,22 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { useTodosStore } from "../store.zustand";
 
 const DetailPage = () => {
-  // let { id } = useParams();
+  let { id } = useParams();
+  const { title, todos, findDataWithId } = useTodosStore((state) => state);
+
+  useEffect(() => {
+    if (id && todos.length > 0) {
+      findDataWithId(id);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [id, todos]);
 
   return (
     <div>
       <h2>DetailPage</h2>
-      <div className="item-title">title asd asd asd asd asd </div>
+      <div className="item-title">{title} </div>
       <Link to="/" className="back-button">
         👈
       </Link>
